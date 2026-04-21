e64c33d7dcd280df82ef258a612ee02588094371
kike <kike@faiadolabs.com>
2026-04-21 13:16:06 +0200

    feat: implement authentication module with user registration and login functionality

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { envs } from './ config/envs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+
+  const logger = new Logger('AuthMS-Main');
+
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      // Objeto de configuración del microservicio
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      }
+    }
+  )
+
+  // Configuración global de pipes
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+  })
+  );
+
+  await app.listen();
+  logger.log(`Auth Microservice running on port ${envs.port}`);
 }
 bootstrap();
